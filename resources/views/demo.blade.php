7a63b0edb32b7b9ce7da19ecc6fe37ec8a3f941f
Micahel-Xcode <lcf_boy@hotmail.com>
2024-01-07 20:27:47 +1300

    pdfjs

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -107,7 +107,7 @@
                 window.eventBus = eventBus;
 
                 const DEFAULT_URL = "{{ $pdfurl }}";
-
+                console.log(DEFAULT_URL);
                 // Loading document.
                 const loadingTask = pdfjsLib.getDocument({
                     url: DEFAULT_URL,
