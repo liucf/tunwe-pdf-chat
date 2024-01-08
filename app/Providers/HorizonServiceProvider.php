d020847230df02eb6d03c24b9b229c0b44d9d67b
Micahel-Xcode <lcf_boy@hotmail.com>
2024-01-08 16:52:29 +1300

    horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -29,7 +29,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user) {
             return in_array($user->email, [
-                //
+                'lcf800@gmail.com'
             ]);
         });
     }
